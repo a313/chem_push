81124dadd1f16d577e02bbd33df75b399cc082f9
tungnt <nguyenthanhtung0207@gmail.com>
2024-10-05 23:16:43 +0700

    add android sign

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:jpush_flutter/jpush_flutter.dart';
 
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -77,7 +78,57 @@ class _MainAppState extends State<MainApp> {
     );
   }
 
-  void initJpush() {}
+  void initJpush() {
+    // final jpush = JPush();
+    // jpush.addEventHandler(
+    //     onReceiveNotification: (Map<String, dynamic> message) async {
+    //   log("onReceiveNotification: $message", name: "JPush");
+    //   notiSource = "JPush onReceiveNotification";
+    //   notiData = message.toString();
+    //   setState(() {});
+    // }, onOpenNotification: (Map<String, dynamic> message) async {
+    //   log("onOpenNotification: $message", name: "JPush");
+    //   notiSource = "JPush onOpenNotification";
+    //   notiData = message.toString();
+    //   setState(() {});
+    // }, onReceiveMessage: (Map<String, dynamic> message) async {
+    //   log("onReceiveMessage: $message", name: "JPush");
+    //   notiSource = "JPush onReceiveMessage";
+    //   notiData = message.toString();
+    //   setState(() {});
+    // }, onReceiveNotificationAuthorization:
+    //         (Map<String, dynamic> message) async {
+    //   log("onReceiveNotificationAuthorization: $message", name: "JPush");
+    //   notiSource = "JPush onReceiveNotificationAuthorization";
+    //   notiData = message.toString();
+    //   setState(() {});
+    // }, onNotifyMessageUnShow: (Map<String, dynamic> message) async {
+    //   log("onNotifyMessageUnShow: $message", name: "JPush");
+    //   notiSource = "JPush onNotifyMessageUnShow";
+    //   notiData = message.toString();
+    //   setState(() {});
+    // }, onInAppMessageShow: (Map<String, dynamic> message) async {
+    //   log("onInAppMessageShow: $message", name: "JPush");
+    //   notiSource = "JPush onInAppMessageShow";
+    //   notiData = message.toString();
+    //   setState(() {});
+    // }, onCommandResult: (Map<String, dynamic> message) async {
+    //   log("onCommandResult: $message", name: "JPush");
+    //   notiSource = "JPush onCommandResult";
+    //   notiData = message.toString();
+    //   setState(() {});
+    // }, onInAppMessageClick: (Map<String, dynamic> message) async {
+    //   log("onInAppMessageClick: $message", name: "JPush");
+    //   notiSource = "JPush onInAppMessageClick";
+    //   notiData = message.toString();
+    //   setState(() {});
+    // }, onConnected: (Map<String, dynamic> message) async {
+    //   log("onConnected: $message", name: "JPush");
+    //   notiSource = "JPush onConnected";
+    //   notiData = message.toString();
+    //   setState(() {});
+    // });
+  }
 
   @override
   void initState() {
